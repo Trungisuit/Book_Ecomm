740854b329b9621064dec3225244ae9add0baf15
chungnek <70212402+chungnek@users.noreply.github.com>
2022-04-16 13:59:06 +0700

    aa

--- app/src/main/java/thanhtrung/android/book_ecomm/MainActivity.java
+++ app/src/main/java/thanhtrung/android/book_ecomm/MainActivity.java
@@ -4,7 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    Homepage homepage = new Homepage();
+    CustomerInformation customerInformation = new CustomerInformation();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
