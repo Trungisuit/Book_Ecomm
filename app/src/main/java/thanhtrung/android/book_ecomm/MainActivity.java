f90c488ea8a148d7a9b2bef0a64770d327fdf784
chungnek <70212402+chungnek@users.noreply.github.com>
2022-04-16 17:22:04 +0700

    dadadada

--- app/src/main/java/thanhtrung/android/book_ecomm/MainActivity.java
+++ app/src/main/java/thanhtrung/android/book_ecomm/MainActivity.java
@@ -1,20 +1,39 @@
 package thanhtrung.android.book_ecomm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
-    Homepage homepage = new Homepage();
+    HomeFragment homepage = new HomeFragment();
     CustomerInformation customerInformation = new CustomerInformation();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        bottomNavigationView = findViewById(R.id.bottom_navi);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homepage).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homepage).commit();
+                    return true;
+
+                }
+                return false;
+            }
+        });
+    };
 }
+
+
