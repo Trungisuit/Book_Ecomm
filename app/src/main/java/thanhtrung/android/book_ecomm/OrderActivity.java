17d67b072b5f38f2f14170cd926a6a7d7aa1a7c1
ntduonggg <thaiduongbt123@gmail.com>
2022-04-17 19:36:18 +0700

    Front end v1

--- app/src/main/java/thanhtrung/android/book_ecomm/OrderActivity.java
+++ app/src/main/java/thanhtrung/android/book_ecomm/OrderActivity.java
@@ -1,18 +1,11 @@
 package thanhtrung.android.book_ecomm;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,36 +13,31 @@ import java.util.List;
 import thanhtrung.android.book_ecomm.adapter.CartAdapter;
 import thanhtrung.android.book_ecomm.model.Cart;
 
-public class CartFragment extends Fragment {
+public class OrderActivity extends AppCompatActivity {
 
-    private RecyclerView rcvCart;
+    private RecyclerView rcvOrder;
     private CartAdapter mCartAdapter;
-    private TextView tvTotal;
-    private Button btnPurchase;
+    private TextView tvTotal, tvDelivery, tvAll;
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_cart, container, false);
-        rcvCart = v.findViewById(R.id.rcv_cart);
-        mCartAdapter = new CartAdapter(this.getContext());
-        tvTotal = v.findViewById(R.id.tv_total);
-        btnPurchase = v.findViewById(R.id.btnPurchase);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this.getContext(), RecyclerView.VERTICAL, false);
-        rcvCart.setLayoutManager(linearLayoutManager);
+        rcvOrder = findViewById(R.id.rcv_order);
+        mCartAdapter = new CartAdapter(this);
+        tvTotal = findViewById(R.id.totalFeeTxt);
+        tvDelivery = findViewById(R.id.deliveryTxt);
+        tvAll = findViewById(R.id.totalTxt);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        rcvOrder.setLayoutManager(linearLayoutManager);
 
         mCartAdapter.setData(getListCart());
-        rcvCart.setAdapter(mCartAdapter);
+        rcvOrder.setAdapter(mCartAdapter);
         tvTotal.setText(getTotal(getListCart()));
-        btnPurchase.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(v.getContext(),OrderActivity.class);
-                startActivity(intent);
-            }
-        });
-        return v;
+        tvDelivery.setText(getDelivery(getListCart()));
+        tvAll.setText(getAll(getListCart()));
     }
 
     private List<Cart> getListCart(){
@@ -69,5 +57,23 @@ public class CartFragment extends Fragment {
             sum += Integer.parseInt(item.getPriceProduct())* Integer.parseInt(item.getQuantityProduct());
         }
         return String.valueOf(sum);
+    }
+
+    private String getDelivery(List<Cart> list){
+        if (list.size() > 5){
+            return "0";
+        }
+        return String.valueOf(list.size()*10000);
+    }
+
+    private String getAll(List<Cart> list){
+        int sum = 0;
+        for (Cart item: list) {
+            sum += Integer.parseInt(item.getPriceProduct())* Integer.parseInt(item.getQuantityProduct());
+        }
+        if (list.size() > 5){
+            return String.valueOf(sum);
+        }
+        return String.valueOf((list.size()*10000) + sum);
     }
 }
